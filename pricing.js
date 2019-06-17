d4f8bad6ceabb2fefb6bb763232978d7cf862849
mpham12 <phamhungmanh37@yahoo.com>
2019-06-17 14:41:38 -0400

    narrow down code

--- pricing.js
+++ pricing.js
@@ -1,43 +1,46 @@
+function calculateMedicalProductPrice(product,selectedOptions,familyMembersToCover){
+  if (fmtc.includes('ee')) {
+    var eeCost = product.costs.find(function (cost) {
+      return cost.role === 'ee'
+    })
+
+    price += eeCost.price
+  }
+
+  if (fmtc.includes('sp')) {
+    var spCost = product.costs.find(function (cost) {
+      return cost.role === 'sp'
+    })
+
+    price += spCost.price
+  }
+
+  if (fmtc.includes('ch')) {
+    var chCost = product.costs.find(function (cost) {
+      return cost.role === 'ch'
+    })
+
+    price += chCost.price
+  }
+
+  if (fmtc.includes('chs')) {
+    var chsCost = product.costs.find(function (cost) {
+      return cost.role === 'chs'
+    })
+
+    price += chsCost.price
+  }
+
+  return parseInt(price * 100) / 100
+
+}
 module.exports.calculateProductPrice = function (product, employee, selectedOptions) {
   var price = 0
   var fmtc = selectedOptions.familyMembersToCover
 
-  switch (product.type) {
-    case 'medical':
-      if (fmtc.includes('ee')) {
-        var eeCost = product.costs.find(function (cost) {
-          return cost.role === 'ee'
-        })
-
-        price += eeCost.price
-      }
-
-      if (fmtc.includes('sp')) {
-        var spCost = product.costs.find(function (cost) {
-          return cost.role === 'sp'
-        })
-
-        price += spCost.price
-      }
-
-      if (fmtc.includes('ch')) {
-        var chCost = product.costs.find(function (cost) {
-          return cost.role === 'ch'
-        })
-
-        price += chCost.price
-      }
-
-      if (fmtc.includes('chs')) {
-        var chsCost = product.costs.find(function (cost) {
-          return cost.role === 'chs'
-        })
-
-        price += chsCost.price
-      }
-
-      return parseInt(price * 100) / 100
-    case 'volLife':
+  function calculateMedicalProductPrice(product,selectedOptions,fmtc){}
+      switch (product.type) {
+      case 'volLife':
       if (fmtc.includes('ee')) {
         var eeCoverage = selectedOptions.coverageLevel.find(function (coverage) {
           return coverage.role === 'ee'
